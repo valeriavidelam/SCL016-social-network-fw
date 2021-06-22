9b8e0f04b33f3e34560521bfc6ed5ccdc344768e
valeriavidelam <v.videlam@gmail.com>
2021-06-22 19:38:00 -0400

    Two buttons are created to do the Authentication  with Firebase / Mail and Googles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+
+
 const Login = () => {
     return ( 
-        <div>
-            Login
+        <div className="mt-5 text-center">
+            <h3 className="text-center">LOGO CHINCHIN</h3>
+            <hr/>
+            <div className="row justify-content-center">
+              <div className="d-grid gap-2 mt-5">
+                <button
+                  className="btn btn-lg btn-light btn-block"
+                  onClick={() => { console.log('Google button clicked') }}
+                  >
+                  Sign in with Google  
+                  </button> 
+                <button 
+                className="btn btn-lg btn-danger btn-block"
+                onClick={()=> {console.log('Mail button clicked')} }>
+                 Sign in with mail
+                </button>
+              </div>
+            </div>
+              
+            
         </div>
      );
 }
