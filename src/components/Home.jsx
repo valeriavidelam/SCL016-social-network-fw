e24bc2536acb2b4d947901a2acc77d8830e74d7f
valeriavidelam <v.videlam@gmail.com>
2021-06-22 20:09:09 -0400

    some basic edits on the components. Sketchs/outline  to check that the routes work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Home = () => {
+
+   const saludo = 'Probando texto con Variable, este debería ser el muro donde van los posts'
+   
+
     return ( 
-        <div>
-            <h3>Página de Inicio</h3>
+        <div className='mt-5'>
+           <h2> {saludo} </h2>
         </div>
      );
 }
  
 export default Home;
+
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,11 @@ import {Link, NavLink} from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <div className= "navbar navbar-dark bg-dark">
-            <Link className="navbar-brand" to="/">CHIN CHIN</Link>
+            <Link className="navbar-brand" to="/">CHIN·CHIN</Link>
             <div className="d-flex">
-                <NavLink className="btn btn-dark mr-2" to="/" exact> Home</NavLink>
-                <NavLink className="btn btn-dark mr-2" to="/login" exact> Login</NavLink>
+                <NavLink className="btn btn-dark mr-2" to="/home" exact> Home</NavLink>
+                <NavLink className="btn btn-dark mr-2" to="/profile" exact> Profile </NavLink>
+                <NavLink className="btn btn-dark mr-2" to="/" exact> Login</NavLink>
                 <button className="btn btn-dark mr-2"> Logout</button>
             </div>
         </div>
