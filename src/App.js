b99e79cd27279f061a4fce040be1cb8b47861baf
VanyCuenca <vanycuenca@gmail.com>
2021-06-23 09:17:48 -0400

    update routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import Wall from './components/Wall'
-import Login from './components/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-
-const App = () => {
-  return ( 
+function App() {
+  
+  return (
     <Router>
-      <div className= "container mt-3">
-
-        <Navbar/>
-
+      <div className="container">
+        <Navbar />
         <Switch>
-          <Route component={Wall} path="/" exact/>
-          <Route component={Login} path="/login" exact/>
+          <Route path="/" exact>
+            Incio...
+          </Route>
+          <Route path="/login">
+           <Login />
+          </Route>
+          <Route path="/admin">
+            admin...
+          </Route>
         </Switch>
       </div>
     </Router>
-   );
+  );
 }
- 
-export default App;
 
+export default App;
