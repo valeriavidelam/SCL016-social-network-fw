86bcef38dd0f38f00f3a594ca9ae02cfb7526a94
VanyCuenca <vanycuenca@gmail.com>
2021-06-23 09:22:08 -0400

    update Route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
 
-
 const Navbar = () => {
-    return ( 
-        <div className= "navbar navbar-dark bg-dark">
-            <Link className="navbar-brand" to="/">CHIN CHIN</Link>
-            <div className="d-flex">
-                <NavLink className="btn btn-dark mr-2" to="/" exact> Home</NavLink>
-                <NavLink className="btn btn-dark mr-2" to="/login" exact> Login</NavLink>
-                <button className="btn btn-dark mr-2"> Logout</button>
-            </div>
+    return (
+        <div className="navbar navbar-dark bg-dar">
+            <Link className="navbar-brand" to="/">CHIN-CHIN</Link>
+            <div>
+                <div className="d-flex">
+                    <NavLink className="btn btn-dar mr-2" to="/" exact>
+                        Inicio
+                    </NavLink>
+                    <NavLink className="btn btn-dar mr-2" to="/admin">
+                        Admin
+                    </NavLink>
+                    <NavLink className="btn btn-dar mr-2" to="/AuthEmail">
+                        Login
+                    </NavLink>
+                </div>
+            </div>            
         </div>
-     );
+    )
 }
- 
-export default Navbar;
+
+export default Navbar
