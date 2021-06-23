e99ca298a7397df3bc2fe33cb3b1c4c8d58d0fef
VanyCuenca <vanycuenca@gmail.com>
2021-06-23 09:21:00 -0400

    user registration button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+//import AuthGoogle from '../Auth/AuthGoogle';
+import AuthEmail from './AuthEmail';
 
 const Login = () => {
     return ( 
-        <div>
-            Login
+        <div className="mt-5 text-center">
+            <h3 className="text-center">LOGO CHINCHIN</h3>
+            <hr/>
+            <div className="row justify-content-center">
+              <div className="d-block gap-2 mt-5">
+                {/* <AuthGoogle/> */}
+                <button 
+                className="btn btn-danger btn-block"
+                onClick={ (AuthEmail) }>
+                 Sign in with mail
+                </button>
+              </div>
+            </div>
         </div>
      );
 }
  
 export default Login;
-
