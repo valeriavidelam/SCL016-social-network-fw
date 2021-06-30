37e2c5cf926536837c5fdb360f24e4ec4e5217bd
VanyCuenca <vanycuenca@gmail.com>
2021-06-30 13:46:49 -0400

    create button to log out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
+import firebase from 'firebase/app';
+import { withRouter } from 'react-router-dom';
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+    //const history = useHistory ();
+
+    const SignOff = () => {
+        firebase.auth().signOut()
+            .then(() => {
+                console.log('esta funcionando')
+                props.history.push('/')
+            })
+    }
+
     return ( 
         
             <div className= "navbar navbar-dark bg-dark">
                 <Link className="navbar-brand" to="/">CHIN·CHIN</Link>
                 <div className="d-flex">
-                     <NavLink className="btn btn-dark mr-2" to="/home" exact> Home</NavLink>
+                     {/* <NavLink className="btn btn-dark mr-2" to="/" exact>Logout</NavLink> */}
+                     {
+                       props.firebase !== null ? (
+                        <NavLink className="btn btn-dark mr-2" to="/home" exact>Home</NavLink>
+                       ) : null
+                     }
+
+                     { props.firebase !== null ? (
+                        <button className="btn btn-dark" onClick={() => SignOff()}>Logout </button>
+                     ) : (
+                        <NavLink className="btn btn-dark mr-2" to="/" exact>Login</NavLink>
+                     )}
                      <NavLink className="btn btn-dark mr-2" to="/profile" exact> Profile </NavLink>
-                     <NavLink className="btn btn-dark mr-2" to="/" exact> Login</NavLink>
-                     <NavLink className="btn btn-dark mr-2" to="/" exact> Logout</NavLink>
                 </div>
             </div>
         
     )
 }
 
-export default Navbar
+export default withRouter (Navbar)
